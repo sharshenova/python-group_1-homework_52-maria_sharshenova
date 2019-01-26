653d1e4f7d325bca58434913b3c65f0b4fd7df4e
Maria Sharshenova <maria@sharshenov.com>
2019-01-27 00:47:07 +0600

    Add Header

--- src/App.js
+++ src/App.js
@@ -2,27 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Header extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="Header">
+        <nav className="navbar navbar-expand-lg navbar-dark">
+          <div className="container">
+            <a className="navbar-brand text-success ml-md-5" href="#">
+              <img src="image/logo1.png" alt="Kinopoisk"></img>
+            </a>
+
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">{this.props.name1}<span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">{this.props.name2}</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">{this.props.name3}</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">{this.props.name4}</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">{this.props.name5}</a>
+                </li>
+              </ul>
+              <a href="#" data-toggle="tooltip" data-placement="bottom" title="Sign up"><i className="far fa-user"></i></a>
+            </div>            
+          </div> 
+        </nav>
       </div>
     );
   }
 }
 
-export default App;
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Header from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Header name1="Главная" name2="Афиша" name3="Фильмы" name4="Рейтинги" name5="Онлайн"/>, document.getElementById('root_header'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
